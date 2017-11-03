ede5d4871cbb45492fc34787fed3adf3ad82066f
Josh winterton <32195114+Winterton-J-BPC-Cyan@users.noreply.github.com>
2017-11-03 09:38:48 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <?php
 $number = rand(1,6);
 echo $number;
-if ($number = 1) {
+if ($number == 1) {
 	echo "Almost snake eyes";
-} elseif ($number = 6) {
+} elseif ($number == 6) {
 	echo "Winner";
 } else {
 	echo "Sorry you lose";
