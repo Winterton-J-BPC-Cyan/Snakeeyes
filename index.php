c764d15df92de975aa075e03fe5e1c8199a325e4
Josh winterton <32195114+Winterton-J-BPC-Cyan@users.noreply.github.com>
2017-11-03 09:42:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 $number = rand(1,6);
 $number2 = rand(1,6);
 echo $number;
-echo "</br>"
-echo $number2
-echo "</br>"
+echo "</br>";
+echo $number2;
+echo "</br>";
 if ($number == 1 or $number2 == 1) {
 	echo "Almost snake eyes";
 } elseif ($number == 6 or $number2 == 6) {
